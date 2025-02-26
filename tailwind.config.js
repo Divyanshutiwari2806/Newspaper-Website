851f9c8a7be8c535434d5ee8d44eed8ab605aedd
Divyanshutiwari2806 <DivyanshuTiwari2222@gmail.com>
2025-02-26 15:51:48 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+import typographyPlugin from '@tailwindcss/typography';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            maxWidth: 'none',
+          },
+        },
+      },
+    },
+  },
+  plugins: [
+    typographyPlugin,
+  ],
+};
